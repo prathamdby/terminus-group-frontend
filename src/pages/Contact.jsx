f0d07acaac34dd2d1e095595c3720a6952d71234
kairvee <imt_2023037@iiitm.ac.in>
2024-11-27 11:30:32 +0530

    fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import ContactSection from "../components/ContactUs";
 const Contact = () => {
   return <>
-  <ContactSection />
+
   <Footer /></>;
 };
 
